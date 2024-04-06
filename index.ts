7e756b7bf225e06a2925ce87b77dcf0d0bb87111
athnxyz <athnapp@gmail.com>
2024-04-06 14:38:06 -0400

    add index.ts

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { 
+  vbutton,
+  vcontainer,
+  vdropdown,
+  vnav,
+  vnotification,
+  vsidebar,
+  vtag,
+  vtext,
+  vtitle
+} from './src/uikit';
+import { UIKitLoader } from './src/uikit.loader';
+
+
+export { 
+  UIKitLoader,
+  vbutton,
+  vcontainer,
+  vdropdown,
+  vnav,
+  vnotification,
+  vsidebar,
+  vtag,
+  vtext,
+  vtitle
+};
--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-
+/*
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
 
@@ -9,3 +9,4 @@ describe('HelloWorld', () => {
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
+*/
