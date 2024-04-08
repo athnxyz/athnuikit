480a87011dfc55d5574aef4d2e9a6530784a209c
athnxyz <athnapp@gmail.com>
2024-04-08 16:59:38 -0400

    add exponential backoff composable

---
+++ src/composables/useExponentialBackoff.ts
@@ -0,0 +1,50 @@
+import type { Ref } from 'vue';
+import { ref } from 'vue';
+
+import { sleep } from '@uikit/utils/Utils';
+
+
+const DEFAULT_DEPTH = 1;
+const DEFAULT_TIMEOUT = 250;
+
+export const useExponentialBackoff = <T extends (...args: any[]) => any>() => {
+  const depth: Ref<number> = ref(DEFAULT_DEPTH);
+  const timeout: Ref<number> = ref(DEFAULT_TIMEOUT);
+  const backoffErr: Ref<Error | null> = ref(null);
+
+  const backoff = (fn: T, retries: number) => {
+    const resetDefaults = () => { 
+      depth.value = DEFAULT_DEPTH;
+      timeout.value = DEFAULT_TIMEOUT;
+    };
+
+    return (...args: Parameters<T>) => {
+      const recursiveBackoff = async (): Promise<ReturnType<T>> => {
+        try {
+          if (depth.value > retries) throw new Error(`exceeded max retries: ${retries}`);
+          return await fn(...args);
+        } catch (err) {
+          if (depth.value > retries) {
+            resetDefaults();
+            backoffErr.value = err as Error;
+            
+            throw err;
+          }
+  
+          const d = depth.value;
+          const currTimeout = timeout.value
+          
+          timeout.value = ((): number => 2 ** (d - 1) * currTimeout)();
+          depth.value++;
+  
+          await sleep(timeout.value);
+          return await recursiveBackoff();
+        }
+      }
+
+      return recursiveBackoff();
+    }
+  };
+
+  return { backoff };
+};
