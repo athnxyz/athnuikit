f34b9a64bb6a1e0df92087d8fa358b06fb6bbed7
athnxyz <athnapp@gmail.com>
2024-04-10 14:17:46 -0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 
 export default defineConfig({
