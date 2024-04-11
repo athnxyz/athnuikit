4319b1a07fd63aa8dc59a3d2050b6d36eba5180b
athnxyz <athnapp@gmail.com>
2024-04-11 12:26:08 -0400

    type

--- src/utils/flow.ts
+++ src/utils/flow.ts
@@ -1,9 +1,9 @@
 type FlowFn<T, U> = (...args: T extends any ? any : never) => U;
-type FinalFlowFn<T extends any[]> = T extends [...infer _, infer Last] ? Last : never;
+type InferLast<T extends any[]> = T extends [...infer _, infer Last] ? Last : never;
 
 
 export const flow = <T extends FlowFn<any, any>[]>(...fns: T) => {
-  return (...args: Parameters<T[0]>): ReturnType<FinalFlowFn<T>> => {
+  return (...args: Parameters<T[0]>): ReturnType<InferLast<T>> => {
     return fns.reduce((acc, fn) => {
       const args = Array.isArray(acc) ? acc : [ acc ];
       return fn(...args);
--- src/views/v-flow-view.vue
+++ src/views/v-flow-view.vue
@@ -20,11 +20,11 @@ console.log(result); // result will be true
 
 const flowFnSignatureImpl = ref(`
 type FlowFn<T, U> = (...args: T extends any ? any : never) => U;
-type FinalFlowFn<T extends any[]> = T extends [...infer _, infer Last] ? Last : never;
+type InferLast<T extends any[]> = T extends [...infer _, infer Last] ? Last : never;
 
 
 export const flow = <T extends FlowFn<any, any>[]>(...fns: T) => {
-  return (...args: Parameters<T[0]>): ReturnType<FinalFlowFn<T>> => {
+  return (...args: Parameters<T[0]>): ReturnType<InferLast<T>> => {
     return fns.reduce((acc, fn) => {
       const args = Array.isArray(acc) ? acc : [ acc ];
       return fn(...args);
@@ -56,7 +56,7 @@ export const flow = <T extends FlowFn<any, any>[]>(...fns: T) => {
 
       <v-input v-model:value="useFlowImpl" disabled></v-input>
       <v-input v-model:value="flowFnSignatureImpl" disabled></v-input>
-      
+
     </v-container>
 
   </v-container>
