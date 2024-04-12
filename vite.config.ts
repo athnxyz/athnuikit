727c15554ec7dc92b07830687af6d3dafc367dda
athnxyz <athnapp@gmail.com>
2024-04-12 16:27:04 -0400

    update peer dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import VueDevTools from 'vite-plugin-vue-devtools';
 import dts from 'vite-plugin-dts';
 
 
